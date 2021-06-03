c0cb8b93ff017ba2cc8d6f65a3436fb71ae1f2fa
AhmedQuas <ahmed.quas@gmail.com>
2021-06-03 18:47:15 -0400

    Week day comparision in 2019 & 2020 plot

--- dataset/plot_diagrams.py
+++ dataset/plot_diagrams.py
@@ -13,26 +13,50 @@ def plot_diagrams(db_file: str):
     pd.options.mode.chained_assignment = None  
 
     plot_month_covid_2019_2020_comparision(conn)
+    plot_week_day_covid_2019_2020_comparision(conn)
 
     conn.close()
 
 def plot_month_covid_2019_2020_comparision(conn: sqlite3):
-    
+
     df = pd.read_sql_query("SELECT year, month_name, accidents, month_order FROM months WHERE year IN('2020', '2019')", conn)
 
     fig, ax = plt.subplots()
-    
+
     df_2019 = df[df['year']==2019].set_index('month_name')
     df_2020 = df[df['year']==2020].set_index('month_name')
 
     df_2019 = df_2019.sort_values('month_order')
 
     df_2020 = df_2020.sort_values('month_order')
-    
+
     ax.bar(df_2019.index, df_2019['accidents'], color='none', edgecolor = 'r')
     ax.bar(df_2020.index, df_2020['accidents'], color='b', alpha=0.7 ,edgecolor = 'b')
     ax.set_title('Powrównanie liczby wypadków w latach 2019 i 2020')
     ax.set_xlabel('Miesiące')
+    ax.set_ylabel('Liczba wypadków')
+    ax.legend(['2019', '2020'])
+    plt.xticks(rotation=45)
+
+    plt.show()
+
+def plot_week_day_covid_2019_2020_comparision(conn: sqlite3):
+
+    df = pd.read_sql_query("SELECT year, week_day_name, accidents, week_day_order FROM week_day WHERE year IN('2020', '2019')", conn)
+
+    fig, ax = plt.subplots()
+
+    df_2019 = df[df['year']==2019].set_index('week_day_name')
+    df_2020 = df[df['year']==2020].set_index('week_day_name')
+
+    df_2019 = df_2019.sort_values('week_day_order')
+
+    df_2020 = df_2020.sort_values('week_day_order')
+
+    ax.bar(df_2019.index, df_2019['accidents'], color='none', edgecolor = 'r')
+    ax.bar(df_2020.index, df_2020['accidents'], color='b', alpha=0.7 ,edgecolor = 'b')
+    ax.set_title('Powrównanie liczby wypadków w latach 2019 i 2020')
+    ax.set_xlabel('Dni tygodnia')
     ax.set_ylabel('Liczba wypadków')
     ax.legend(['2019', '2020'])
     plt.xticks(rotation=45)
