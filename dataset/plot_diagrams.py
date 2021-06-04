a0aeb37892a7760aab8fc925767b1ccbb3933f68
AhmedQuas <ahmed.quas@gmail.com>
2021-06-04 19:45:26 -0400

    Accidents by driver age - 2 versions

--- dataset/plot_diagrams.py
+++ dataset/plot_diagrams.py
@@ -12,8 +12,10 @@ def plot_diagrams(db_file: str):
 
     pd.options.mode.chained_assignment = None  
 
-    plot_month_covid_2019_2020_comparision(conn)
-    plot_week_day_covid_2019_2020_comparision(conn)
+    #plot_month_covid_2019_2020_comparision(conn)
+    #plot_week_day_covid_2019_2020_comparision(conn)
+    accidents_by_driver_age(conn)
+    accidents_by_driver_age_v2(conn)
 
     conn.close()
 
@@ -60,5 +62,62 @@ def plot_week_day_covid_2019_2020_comparision(conn: sqlite3):
     ax.set_ylabel('Liczba wypadków')
     ax.legend(['2019', '2020'])
     plt.xticks(rotation=45)
+
+    plt.show()
+
+def accidents_by_driver_age(conn: sqlite3):
+
+    df = pd.read_sql_query("SELECT year, age_name, accidents, age_order FROM driver_age", conn)
+
+    fig, ax = plt.subplots()
+
+    df.set_index('year')
+    df = df.sort_values(['year', 'age_order'], ascending=[True, True])
+
+    groups = []
+
+    for i in range(1,9):
+        temp = df[df['age_order']==i]
+        groups.append(temp['accidents'].values.tolist())
+
+    group_labels = ['0-6','7-14','15-17','18-24','25-39','40-59','60+','b/d']
+
+    df = pd.DataFrame(groups, index=group_labels).T
+
+    df.index = [2015, 2016, 2017, 2018, 2019, 2020]
+
+    p1 = pd.concat([df], axis=1).plot.bar(grid=True)
+
+    ax.set_title('Powrównanie liczby wypadków w zależności od grupy wiekowej')
+    ax.set_xlabel('Grupy wiekowe')
+    ax.set_ylabel('Liczba wypadków')
+    plt.xticks(rotation=45)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+
+    plt.show()
+
+
+def accidents_by_driver_age_v2(conn: sqlite3):
+
+    df = pd.read_sql_query("SELECT year, age_name, accidents, age_order FROM driver_age", conn)
+
+    fig, ax = plt.subplots()
+
+    df.set_index('year')
+    df = df.sort_values(['year', 'age_order'], ascending=[True, True])
+
+    groups = []
+
+    for i in [2015, 2016, 2017, 2018, 2019, 2020]:
+        temp = df[df['year']==i]
+        groups.append(temp['accidents'].values.tolist())
+
+    group_labels = [2015, 2016, 2017, 2018, 2019, 2020]
+
+    df = pd.DataFrame(groups, index=group_labels).T
+
+    df.index = ['0-6','7-14','15-17','18-24','25-39','40-59','60+','b/d']
+
+    pd.concat([df], axis=1).plot.bar(grid=True)
 
     plt.show()
